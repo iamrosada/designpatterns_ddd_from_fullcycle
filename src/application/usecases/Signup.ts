c9d3db90bb4b465d3c35612b1227105ee4649041
iamrosada <luisrosada2018@gmail.com>
2023-05-06 04:26:32 +0300

    feat: created signup usecase

---
+++ src/application/usecases/Signup.ts
@@ -0,0 +1,18 @@
+import User from "../../domain/entity/User";
+import UserRepository from "../repository/UserRepository";
+
+export default class Signup {
+  constructor(readonly userRepository: UserRepository) {}
+
+  async execute(input: Input): Promise<void> {
+    const user = new User(input.name, input.email, input.password, input.age);
+    await this.userRepository.save(user);
+  }
+}
+
+type Input = {
+  name: string;
+  email: string;
+  password: string;
+  age: number;
+};
