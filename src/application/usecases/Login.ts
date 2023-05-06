765d8d4311854a1c7a5ef64dcdf97fed8f170f12
iamrosada <luisrosada2018@gmail.com>
2023-05-06 21:14:36 +0300

    feat: created usecase for login

--- src/application/usecases/Login.ts
+++ src/application/usecases/Login.ts
@@ -3,10 +3,24 @@ import UserRepository from "../repository/UserRepository";
 export default class Login {
   constructor(readonly userRepository: UserRepository) {}
 
-  async execute(input: Input): Promise<Output> {}
+  async execute(input: Input): Promise<Output> {
+    const user = await this.userRepository.getByEmail(input.email);
+    if (!user) throw new Error("Authentication failed");
+    if (user?.password !== input.password)
+      throw new Error("Authentication failed");
+    return {
+      name: user.name,
+      token: "123456",
+    };
+  }
 }
 
 type Input = {
   email: string;
   password: string;
+};
+
+type Output = {
+  name: string;
+  token: string;
 };
